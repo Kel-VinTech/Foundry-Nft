f350eedb0b237db77371b2c3d6cb8b824ba9b9f5
Kel-VinTech <mondaykelvintech@gmail.com>
2023-08-19 13:00:46 -0400

    added new script

---
+++ script/DeployBasicNft.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNFT.sol";
+
+contract DeployBasicNft is Script {
+    function run() external returns (BasicNft) {
+        vm.startBroadcast();
+        BasicNft basicNft = new BasicNft();
+        vm.stopBroadcast();
+    }
+}
